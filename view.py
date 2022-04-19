1484ecc8a9c0595d3a4d09c43fab62de77635b6d
Paulo Correia <30002232@students.ual.pt>
2022-04-19 20:10:15 +0100

    inicial

--- view.py
+++ view.py
@@ -5,5 +5,8 @@ def main():
     while True:
         comandos = input().split()
         op = comandos[0].upper()
+        parametros = comandos[1:]
         if(op == 'RIP'):
-             print (op)
+            ll.insert_at_start(parametros)
+            ll.traverse_list()
+          
