3be509b5b673707651104b51479bfba94489ec98
Paulo Correia <30002232@students.ual.pt>
2022-04-19 21:35:30 +0100

    inicial

--- view.py
+++ view.py
@@ -7,6 +7,37 @@ def main():
         op = comandos[0].upper()
         parametros = comandos[1:]
         if(op == 'RIP'):
-            ll.insert_at_start(parametros)
+            ll.insert_at_start(parametros[0])
             ll.traverse_list()
-          
+        elif(op == 'RPF'):
+            ll.insert_at_end(parametros[0])
+            ll.traverse_list()
+        elif(op == 'RPDE'):
+            ll.insert_after_item(parametros[1], parametros[0])
+            ll.traverse_list()
+        elif(op == 'RPAE'):
+            ll.insert_before_item(parametros[1], parametros[0])
+            ll.traverse_list()
+        elif(op == 'RPI'):
+            ll.insert_at_index(int(parametros[1]), parametros[0])
+            ll.traverse_list()
+        elif(op == 'VNE'):
+            print(f"O número de elementos são {ll.get_count()}")
+        elif(op == 'VP'):
+            if(ll.search_item(parametros[0])):
+                print(f"O país {parametros[0]} encontra-se na lista.")
+            else:
+                print(f"O país {parametros[0]} não se encontra na lista.")
+        elif(op == 'EPE'):
+            primeiro_elemento = ll.start_node
+            ll.delete_at_start()
+            print(f"O país {primeiro_elemento} foi eliminado da lista.")
+        elif(op == 'EUE'):
+            ll.delete_at_end()
+            print(f"O país {parametros[0]} foi eliminado da lista.")
+        elif(op == 'EP'):
+            if(ll.search_item(parametros[0])):
+                ll.delete_element_by_value(parametros[0])
+                print(f"O país {parametros[0]} foi eliminado da lista.")
+            else:
+                print(f"O país {parametros[0]} não se encontra na lista.")
